cdf5e5290e1151174bf43e2b24e6134bd9302dfe
Charles <adams.charles00@gmail.com>
2017-10-17 17:42:16 -0400

    initial

---
+++ script.js
@@ -0,0 +1,30 @@
+var nav = document.getElementById("nav");
+var anchor = nav.getElementsByTagName("a");
+var icon = document.getElementById("icon");
+
+ (function navSpin() {
+   spin = setInterval(function() {
+   if(icon.className != "rotate") {
+    icon.className = "rotate";
+    setTimeout(function() {
+      icon.className = "";
+    }, 1500);
+  }
+}, 1499);
+  icon.addEventListener("click", function() {
+    clearInterval(spin);
+  });
+})();
+
+ icon.addEventListener("click", function() {
+   nav.style.display = "block";
+   icon.style.display = "none";
+   Array.prototype.forEach.call(anchor, function(a) {
+     if(a.style.visibility != "visible") {
+       a.style.visibility = "visible";
+     }
+     else if(a.style.visibility = "visible") {
+       a.style.visibility = "hidden";
+     }
+   });
+  });
