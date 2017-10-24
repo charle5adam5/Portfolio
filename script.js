fa2d7c8b5912256b6fd2d541db80884212564a7f
Charles <adams.charles00@gmail.com>
2017-10-24 16:15:25 -0400

    edits

--- script.js
+++ script.js
@@ -11,20 +11,4 @@ var icon = document.getElementById("icon");
     }, 1500);
   }
 }, 1499);
-  icon.addEventListener("click", function() {
-    clearInterval(spin);
-  });
 })();
-
- icon.addEventListener("click", function() {
-   nav.style.display = "block";
-   icon.style.display = "none";
-   Array.prototype.forEach.call(anchor, function(a) {
-     if(a.style.visibility != "visible") {
-       a.style.visibility = "visible";
-     }
-     else if(a.style.visibility = "visible") {
-       a.style.visibility = "hidden";
-     }
-   });
-  });
